cb2edc6c97df19d129195682a64075d79eea0968
Dwain Kyle Anderson <dwainanderson@meta.com>
2025-06-17 19:09:29 -0700

    [feature] Implement controllers and additional backend logic

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const server = require('./src/server')
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, (req, res, next) => {
+  console.log(`Server is running on port ${PORT}`)
+
+})
---
+++ server/src/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const BoardController = require('./controllers/BoardController');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+
+server.use('/*', (req, res, next) => {
+    next();
+});
+
+server.get('/api/boards', async (req, res) => {
+    try {
+        const boards = await BoardController.getAll();
+        res.json({ message: 'success', boards, status: 200 });
+    } catch (error) {
+        res.status(500).json({ message: 'error', error: error.message, status: 500 });
+    }
+});
+
+server.post('/api/boards', async (req, res) => {
+    try {
+        const board = await BoardController.create(req.body);
+        res.json({ message: 'success', board, status: 200 });
+    } catch (error) {
+        res.status(500).json({ message: 'error', error: error.message, status: 500 });
+    }
+})
+
+server.delete('/api/boards/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const board = await BoardController.delete(id);
+        res.json({ message: 'success', board, status: 200 });
+    } catch (error) {
+        res.status(500).json({ message: 'error', error: error.message, status: 500 });
+    }
+})
+
+
+server.use('/*', (req, res, next) => {
+    next({ status: 404, message: 'Not found' })
+})
+
+
+server.use((err, req, res, next) => {
+    const { message, status = 500 } = err
+    res.status(status).json({ message })
+})
+
+module.exports = server;
