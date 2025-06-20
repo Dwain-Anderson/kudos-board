40d475fcd96c7a829f9315eba5268617e2481b03
Dwain Kyle Anderson <dwainanderson@meta.com>
2025-06-20 14:23:50 -0700

    [fix] remove unused context

--- client/src/shared/components/Partials.jsx
+++ client/src/shared/components/Partials.jsx
@@ -1,21 +1,10 @@
 import "./Partials.css";
-import { useContext } from "react";
-import { ColorThemeContext } from "../context/ColorThemeContext";
 
 function Footer() {
-  const { dark, updateColorTheme } = useContext(ColorThemeContext);
 
-  const handleClick = (event) => {
-    event.preventDefault();
-    console.log("Clicked");
-    console.log(dark);
-    updateColorTheme();
-    console.log(dark);
-  }
 
   return (
     <footer>
-      <button onclick={handleClick}>Toggle {dark ? "Dark" : "Light"} Mode</button>
       <p>© 2025 Kudos Board</p>
     </footer>
   );
